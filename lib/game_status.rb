33f9748ffa23e10c9dc55f0e835e2e0f92217d9a
Janis Lebron <janislebron@gmail.com>
2020-04-28 02:37:50 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,14 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
+  "X won in the top row"
+end
+
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5]  # Middle row
+  # ETC, an array for each win combination
+]
+
 # Define your WIN_COMBINATIONS constant
